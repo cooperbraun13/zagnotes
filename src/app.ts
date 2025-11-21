088c0776371fb65e396a0aee8161540f3450c083
cooperbraun13 <cooperjbraun13@gmail.com>
2025-11-20 18:03:31 -0800

    feat: set up basic app and server files for initializing express server, setting up middleware and static file handling

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+// middleware to parse form and json bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static files
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
+
+// home page
+app.get("/", (req, res) => {
+  const html = renderHome();
+  res.send(html);
+});
+
+// mount routers (will implement routes later)
+
+export default app;
